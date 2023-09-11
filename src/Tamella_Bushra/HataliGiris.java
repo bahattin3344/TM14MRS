3c5c4c5c2426c147fc570148f106da4784faaec6
Lenovo <busranuro.93@gmail.com>
2023-09-11 23:37:07 +0300

    HataliGiris düzenlendi

--- src/Tamella_Bushra/HataliGiris.java
+++ src/Tamella_Bushra/HataliGiris.java
@@ -4,16 +4,18 @@ import Utility.BaseDriver;
 import Utility.MyFunc;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class HataliGiris extends BaseDriver{
 
 
-    @Test(groups = {"Smoke Test"})
-        public void hataliGirisTest(){
+    @Test(groups = {"Smoke Test"},dataProvider = "UserData")
+        public void hataliGirisTest(String username, String password){
 
 
             Actions aksiyonlar=new Actions(driver);
@@ -34,12 +36,12 @@ public class HataliGiris extends BaseDriver{
             MyFunc.Bekle(2);
 
 
-            WebElement username=driver.findElement(By.name("username"));
-            username.sendKeys("bushra");
+            WebElement username1=driver.findElement(By.name("username"));
+            username1.sendKeys(username+ Keys.ENTER);
             MyFunc.Bekle(2);
 
-            WebElement password=driver.findElement(By.name("password"));
-            password.sendKeys("122345");
+            WebElement password1=driver.findElement(By.name("password"));
+            password1.sendKeys(password+Keys.ENTER);
             MyFunc.Bekle(2);
 
             WebElement logInB=driver.findElement(By.id("loginButton"));
@@ -62,22 +64,20 @@ public class HataliGiris extends BaseDriver{
             Assert.assertTrue(onaylama2.getText().contains("Invalid username/password. Please try again."));
             MyFunc.Bekle(2);
 
-
-            WebElement username2=driver.findElement(By.name("username"));
-            username2.sendKeys("admin");
-            MyFunc.Bekle(2);
-
-            WebElement password2=driver.findElement(By.name("password"));
-            password2.sendKeys("Admin123");
-            MyFunc.Bekle(2);
-
-            WebElement location2=driver.findElement(By.id("Inpatient Ward"));
-            aksiyonlar.moveToElement(location2).click().build().perform();
-            MyFunc.Bekle(2);
-
-            WebElement logIn3=driver.findElement(By.id("loginButton"));
-            aksiyonlar.moveToElement(logIn3).click().build().perform();
-            MyFunc.Bekle(2);
-
         }
+
+    @DataProvider
+    public Object[][] UserData(){
+        Object[][] data={
+                {"bushra","11111"},
+                {"tamella", "11111"},
+                {"bilgehan","11111"},
+                {"deniz", "11111"},
+                {"bilal","11111"},
+                {"hakan","11111"}
+        };
+
+        return data;
+    }
+
     }
