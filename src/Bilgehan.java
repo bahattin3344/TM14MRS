d7f1961d069b84fe0b4a607db14559b52e49cc62
bilgehanUnsal <bilgehanunsal@outlook.com.tr>
2023-09-12 23:58:25 +0300

    Groupslar ve TM14MRS_6 ve TM14MRS_6_Negative tamamlandı.

--- src/Bilgehan.java
+++ src/Bilgehan.java
@@ -1,4 +1,5 @@
 import Utility.BaseDriver;
+import Utility.MyFunc;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -7,9 +8,11 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class Bilgehan extends BaseDriver {
 
-    @Test
+    @Test(groups = {"Smoke"})
     public void TM14MRS_5(){
         driver.get("https://demo.openmrs.org/openmrs/login.htm");
         Actions driverAksiyon = new Actions(driver);
@@ -35,7 +38,7 @@ public class Bilgehan extends BaseDriver {
 
     }
 
-    @Test
+    @Test(groups = {"PatientManagement"})
     public void TM14MRS_6(){
         driver.get("https://demo.openmrs.org/openmrs/login.htm");
         Actions driverAksiyon = new Actions(driver);
@@ -53,9 +56,40 @@ public class Bilgehan extends BaseDriver {
         findPatientRecordButton.click();
 
         WebElement searchBox = driver.findElement(By.cssSelector("[placeholder = 'Search by ID or Name']"));
-        searchBox.sendKeys("rohit sharma");
+        searchBox.sendKeys("Sam Freeman");
+
+        MyFunc.Bekle(5);
 
         WebElement patient = driver.findElement(By.xpath("//tr[@class='odd']/td"));
         driverAksiyon.moveToElement(patient).click(patient).build().perform();
+
+        List<WebElement> recentVisits = driver.findElements(By.xpath("//div[@class='info-body']/visitbyencountertype/ul/li"));
+
+        for (WebElement e : recentVisits){
+            System.out.println(e.getText());
+        }
+    }
+
+    @Test (groups = {"PatientManagement"})
+    public void TM14MRS_6_Negative(){
+        driver.get("https://demo.openmrs.org/openmrs/login.htm");
+        Actions driverAksiyon = new Actions(driver);
+
+        WebElement usernameBox = driver.findElement(By.name("username"));
+        usernameBox.sendKeys("admin");
+
+        WebElement laboratoryLink = driver.findElement(By.id("Laboratory"));
+        laboratoryLink.click();
+
+        WebElement passwordBox = driver.findElement(By.id("password"));
+        passwordBox.sendKeys("Admin123", Keys.ENTER);
+
+        WebElement findPatientRecordButton = driver.findElement(By.id("coreapps-activeVisitsHomepageLink-coreapps-activeVisitsHomepageLink-extension"));
+        findPatientRecordButton.click();
+
+        WebElement searchBox = driver.findElement(By.cssSelector("[placeholder = 'Search by ID or Name']"));
+        searchBox.sendKeys("mesut komiser");
+
+        Assert.assertTrue(driver.findElement(By.xpath("//td[text()='No matching records found']")).getText().contains("No matching"));
     }
 }
